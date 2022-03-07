72e1a683791ebc428bf294436c057a4faa4d568b
Jonas Otto <edwin.otto@uni-ulm.de>
2022-03-07 16:55:02 +0100

    add experimental hw status visualization

--- setup.py
+++ setup.py
@@ -1,11 +1,36 @@
+import subprocess
+import sys
+
 from setuptools import setup
+import setuptools.command.build_py
 
 package_name = 'spatz_display_rqt'
+
+# List of all .proto files
+proto_dir = "external/protobuf_types"
+proto_src = [proto_dir + '/container.proto',
+             proto_dir + '/controlstate.proto',
+             proto_dir + '/hwhealth.proto',
+             proto_dir + '/lights.proto',
+             proto_dir + '/sensor.proto',
+             proto_dir + '/setpoint.proto',
+             proto_dir + '/time.proto',
+             proto_dir + '/util.proto']
+proto_dest_dir = "src/spatz_display_rqt/generated"
+
+
+class BuildWithProtoBuf(setuptools.command.build_py.build_py):
+    def run(self):
+        sys.stderr.write("Running protoc generation")
+        for proto_file in proto_src:
+            subprocess.run(["protoc", f"-I={proto_dir}", f"--python_out={proto_dest_dir}", f"{proto_file}"])
+        setuptools.command.build_py.build_py.run(self)
+
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, package_name + ".generated"],
     package_dir={'': 'src'},
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -16,6 +41,9 @@ setup(
         ('share/' + package_name, ['plugin.xml']),
     ],
     install_requires=['setuptools'],
+    cmdclass={
+        'build_py': BuildWithProtoBuf,
+    },
     zip_safe=True,
     maintainer='Jonas Otto',
     maintainer_email='jonas@jonasotto.com',
--- src/spatz_display_rqt/spatz_display_widget.py
+++ src/spatz_display_rqt/spatz_display_widget.py
@@ -1,7 +1,9 @@
 import os
+from typing import Dict
 
-from PyQt5.QtCore import (pyqtSlot)
-from PyQt5.QtWidgets import QWidget
+import spatz_display_rqt.generated.hwhealth_pb2 as hwhealth_pb2
+from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QCheckBox
 from PyQt5.uic import loadUi
 from ament_index_python import get_resource
 
@@ -13,7 +15,38 @@ class DisplayWidget(QWidget):
         _, package_path = get_resource('packages', 'spatz_display_rqt')
         ui_file = os.path.join(package_path, 'share', 'spatz_display_rqt', 'resource', 'display.ui')
         loadUi(ui_file, self)
+        self.hw_health_checkboxes: Dict[str, QCheckBox] = {}
+        hwhealth = hwhealth_pb2.HwHealth()
+        hwhealth.indicator_front = True
+        hwhealth.gyro = False
+        hwhealth.vesc_tx_rear_right = False
+        hwhealth.vesc_rx_rear_right = True
+        print(hwhealth.SerializeToString())
+        self.update_hw_status(b'8\x01')
 
     @pyqtSlot()
     def on_go_button_clicked(self):
         print("Go!")
+        self.update_hw_status(b'8\x01\xa0\x01\x01')
+
+    def update_hw_status(self, data):
+        hw_health = hwhealth_pb2.HwHealth()
+        hw_health.ParseFromString(data)
+
+        fields = [fd.name for fd in hw_health.DESCRIPTOR.fields]
+        layout: QVBoxLayout = self.hardware_status_layout
+        for field_name in fields:
+            value = getattr(hw_health, field_name)
+            if not isinstance(value, bool):
+                print(f"Field {field_name} in hw status message is not boolean! Value: {value}")
+                continue
+            if field_name in self.hw_health_checkboxes:
+                checkbox = self.hw_health_checkboxes[field_name]
+            else:
+                checkbox = QCheckBox(field_name)
+                checkbox.setAttribute(Qt.WA_TransparentForMouseEvents)
+                checkbox.setFocusPolicy(Qt.NoFocus)
+                layout.insertWidget(layout.count() - 1, checkbox)
+                self.hw_health_checkboxes[field_name] = checkbox
+
+            checkbox.setChecked(value)
